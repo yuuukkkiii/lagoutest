9296d7b2b8f2833c8248604c27d489082d140f19
zhaih7 <zhaih7@chinaunicom>
2022-02-07 11:44:49 +0800

    继承IService

--- mybatisPlusTest/src/main/java/com/example/zh/mybatisplustest/mapper/UserInfoBaseMapper.java
+++ mybatisPlusTest/src/main/java/com/example/zh/mybatisplustest/mapper/UserInfoBaseMapper.java
@@ -2,13 +2,13 @@ package com.example.zh.mybatisplustest.mapper;
 
 import com.example.zh.mybatisplustest.entity.UserInfoBase;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Entity com.example.zh.mybatisplustest.entity.UserInfoBase
  */
-
+@Mapper
 public interface UserInfoBaseMapper extends BaseMapper<UserInfoBase> {
-
 }
 
 
--- mybatisPlusTest/src/main/java/com/example/zh/mybatisplustest/service/impl/MybatisPlusServiceImpl.java
+++ mybatisPlusTest/src/main/java/com/example/zh/mybatisplustest/service/impl/MybatisPlusServiceImpl.java
@@ -1,7 +1,15 @@
 package com.example.zh.mybatisplustest.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.example.zh.mybatisplustest.base.BaseResponse;
+import com.example.zh.mybatisplustest.contant.ResponseEnum;
+import com.example.zh.mybatisplustest.entity.UserInfoBase;
+import com.example.zh.mybatisplustest.service.UserBaseService;
+import com.example.zh.mybatisplustest.dto.req.UserBaseReq;
+import com.example.zh.mybatisplustest.mapper.UserInfoBaseMapper;
 import com.example.zh.mybatisplustest.service.MybatisPlusService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,11 +20,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MybatisPlusServiceImpl implements MybatisPlusService {
+    @Autowired
+    UserInfoBaseMapper userInfoBaseMapper;
+
+    @Autowired
+    UserBaseService userBaseService;
     /**
      * @return mybatis-plus查询的使用结果
      */
     @Override
-    public BaseResponse<String> selectResult() {
+    public BaseResponse<String> selectResult(UserBaseReq req) {
+        Integer code=req.getCode();
+        UserInfoBase target=new UserInfoBase();
+        BeanUtils.copyProperties(req,target);
+        if(code==null){
+            return BaseResponse.fail(ResponseEnum.RESP_1.getCode(),ResponseEnum.RESP_1.getMsg());
+        }else if(code==1){
+            return BaseResponse.success(JSON.toJSONString(userInfoBaseMapper.selectById(1)));
+        }else if(code==2){
+            return BaseResponse.success(JSON.toJSONString(userBaseService.save(target)));
+        }
         return null;
     }
 }
