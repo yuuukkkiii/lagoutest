7f4086f34936a99801625060a45a71d64ec30eca
zhaih7 <zhaih7@chinaunicom>
2022-02-11 17:29:05 +0800

    添加请求日志库相关

---
+++ aopLogParamTest/src/main/java/com/example/zh/aoplogparamtest/mapper/UserInfoReqLogMapper.java
@@ -0,0 +1,15 @@
+package com.example.zh.aoplogparamtest.mapper;
+
+import com.example.zh.aoplogparamtest.entity.UserInfoReqLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity com.example.zh.aoplogparamtest.entity.UserInfoReqLog
+ */
+public interface UserInfoReqLogMapper extends BaseMapper<UserInfoReqLog> {
+
+}
+
+
+
+
---
+++ aopLogParamTest/src/main/java/com/example/zh/aoplogparamtest/service/UserInfoReqLogService.java
@@ -0,0 +1,11 @@
+package com.example.zh.aoplogparamtest.service;
+
+import com.example.zh.aoplogparamtest.entity.UserInfoReqLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ *
+ */
+public interface UserInfoReqLogService extends IService<UserInfoReqLog> {
+
+}
